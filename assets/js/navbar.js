67f8e8854ab84154cda43a8883c7e1908b6cb52f
guluehmedova <guliehmedova19@gmail.com>
2023-06-01 11:33:33 +0400

    read reviews page

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,9 +7,34 @@ window.onscroll = () => {
     navbar.classList.remove("active");
 }
 
-// document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
-//     inputNumber.oninput = () => {
-//         if (inputNumber.value.length > inputNumber.maxLength)
-//             inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
-//     }
-// });
+
+
+//DROPDOWN
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
